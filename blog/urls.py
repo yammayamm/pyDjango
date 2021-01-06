2f5d56516ba23b2ce6156fb4e00bc2e84eb73b5c
Doyeon <eded0902@naver.com>
2021-01-06 23:39:33 +0900

    Upload first&second django app
    bookmark and blog app

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from blog import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.PostLV.as_view(), name='index'),
+    path('post/', views.PostLV.as_view(), name='post_list'),
+    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+    path('archive/', views.PostAV.as_view(), name='post_archive'),
+    path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
+    path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
+    path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
+    path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
+]
---
+++ bookmark/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+class BookmarkLV(ListView):
+    model = Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
